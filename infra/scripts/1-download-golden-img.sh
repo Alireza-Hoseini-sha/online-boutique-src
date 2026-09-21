c066a9bbe9cb67ec93c2a1def9fc24dcb0917d18
Alireza Hoseini <AlirezaHoseiniSha@gmail.com>
2026-09-21 10:23:03 +0330

    removed trivy for now

--- infra/scripts/1-download-golden-img.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-wget https://cloud-images.ubuntu.com/releases/resolute/release-20260823/ubuntu-26.04-server-cloudimg-amd64.img
--- infra/scripts/3-provision-all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-./create-vm.sh vm-ansible-controller 2048 1 15 192.168.122.10
-./create-vm.sh k8s-master            4096 2 20 192.168.122.11
-./create-vm.sh k8s-worker-01         6144 2 20 192.168.122.12
-./create-vm.sh k8s-worker-02         6144 2 20 192.168.122.13
-./create-vm.sh vm-gitlab             8192 4 40 192.168.122.14
